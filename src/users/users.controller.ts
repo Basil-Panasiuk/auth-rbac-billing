fc5e4d2a91d5c3264a068617d47c5b101813d881
Basil-Panasiuk <v.panasiuk.job@gmil.com>
2024-07-17 00:58:23 +0300

    Fix: update user api docs

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,10 +13,27 @@ import { UsersService } from './users.service';
 import { ValidationExeptionFilter } from 'src/common/filters/validation-exeption.filter';
 import { UserParamDto } from './dto/user-param.dto';
 import { ActiveUserData } from 'src/iam/interfaces/active-user-data.interface';
-import { ApiCookieAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiCookieAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { PaginatedTransactionsResponseDto } from 'src/common/dto/paginated-transactions-response.dto';
+import { PaginationValidationErrorsDto } from 'src/common/dto/pagination-validation-errors.dto';
+import { ApiUnauthorizedResponseConfig } from 'src/iam/authentication/common/apiUnauthorizedResponse.config';
+import { Role } from './enums/role.enum';
 
 @ApiTags('user')
+@ApiResponse({
+  status: 401,
+  description: 'Unauthorized',
+  content: {
+    'application/json': {
+      examples: ApiUnauthorizedResponseConfig,
+    },
+  },
+})
 @ApiCookieAuth()
 @Controller('user')
 export class UsersController {
@@ -24,10 +41,25 @@ export class UsersController {
 
   @Get('deposits')
   @UseFilters(new ValidationExeptionFilter('Validation failed'))
+  @ApiOperation({ summary: 'Get paginated deposits of user' })
   @ApiResponse({
     status: 200,
     description: 'List of deposits with pagination',
     type: PaginatedTransactionsResponseDto,
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Page not found',
+    content: {
+      'application/json': {
+        example: { message: 'Page not found' },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 422,
+    description: 'Validation failed',
+    type: PaginationValidationErrorsDto,
   })
   async getDeposits(
     @Query() paginationQueryDto: PaginationQueryDto,
@@ -37,10 +69,25 @@ export class UsersController {
   }
 
   @Get('transfers')
+  @ApiOperation({ summary: 'Get paginated transfers of user' })
   @ApiResponse({
     status: 200,
     description: 'List of transfers with pagination',
     type: PaginatedTransactionsResponseDto,
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Page not found',
+    content: {
+      'application/json': {
+        example: { message: 'Page not found' },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 422,
+    description: 'Validation failed',
+    type: PaginationValidationErrorsDto,
   })
   @UseFilters(new ValidationExeptionFilter('Validation failed'))
   async getTransfers(
@@ -51,12 +98,36 @@ export class UsersController {
   }
 
   @Patch(':id/deactivate')
+  @ApiOperation({ summary: 'Deactivation of user' })
   @ApiResponse({
     status: 200,
     description: 'User deactivation',
     content: {
       'application/json': {
-        example: { message: 'User has been deactivated' },
+        example: {
+          message: 'User has been deactivated',
+          user: {
+            id: 1,
+            email: 'example@gmailcom',
+            role: Role.REGULAR,
+            balance: 9.99,
+            isActive: false,
+          },
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Invalid user id',
+    content: {
+      'application/json': {
+        example: {
+          message: 'The user with the requested id does not exist',
+          fails: {
+            userId: ['The user id must be an integer.'],
+          },
+        },
       },
     },
   })
@@ -66,6 +137,15 @@ export class UsersController {
     content: {
       'application/json': {
         example: { message: 'User has not correct rights' },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'User not found',
+    content: {
+      'application/json': {
+        example: { message: 'User #{id} not found' },
       },
     },
   })
